5268923d20867c5354c3c7c4ddf9ccb145fad6c2
EdbertJunus05 <edbertyang88@gmail.com>
2022-06-15 16:35:39 +0700

    integrate with firebase

--- app/src/main/java/com/example/todomobile/MainActivity.java
+++ app/src/main/java/com/example/todomobile/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.todomobile;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> insertedData = new HashMap<>();
+        insertedData.put("name", "WAKANDA");
+        insertedData.put("age", 21);
+        insertedData.put("gender", "Male");
+
+        //Jika nama collectionPath asal, dia auto kebuat di db
+        //Jika db tidak diterima, dia auto id
+//        db.collection("students")
+//                .document("2201767623")
+//                .set(insertedData);
+        db.collection("students")
+                .document()
+                .set(insertedData);
+
+        db.collection("students").add(insertedData);
     }
 }
