1fa95b25e34ebd94f0f66f4738d847ac976b78d2
Nathanael <naelj15@gmail.com>
2022-06-16 00:14:54 +0700

    update main activity

--- app/src/main/java/com/example/todomobile/MainActivity.java
+++ app/src/main/java/com/example/todomobile/MainActivity.java
@@ -9,7 +9,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageButton;
+
+import com.example.todomobile.database.UserHelper;
+import com.example.todomobile.model.User;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,23 +32,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        db = FirebaseFirestore.getInstance();
 
-        Map<String, Object> insertedData = new HashMap<>();
-        insertedData.put("name", "WAKANDA");
-        insertedData.put("age", 21);
-        insertedData.put("gender", "Male");
+        UserHelper userHelper = new UserHelper("AdminId");
+        Vector<User> users = userHelper.showUserList();
+//        userHelper.showUserList( new UserHelper.FirestoreCallback(){
+//            @Override
+//            public void onCallback(Vector<User> userVector) {
+//                for (User user : userVector) {
+//                    users.add(user);
+//                    Log.d("Data", user.getUserName());
+//                }
+//            }
+//        });
 
-        //Jika nama collectionPath asal, dia auto kebuat di db
-        //Jika db tidak diterima, dia auto id
-//        db.collection("students")
-//                .document("2201767623")
-//                .set(insertedData);
-        db.collection("students")
-                .document()
-                .set(insertedData);
+        Integer totalUser = users.size();
+        System.out.println("Total User " + totalUser);
 
-        db.collection("students").add(insertedData);
+        for (User user : users) {
+            Log.d("Data di Loop", user.getUserName());
+        }
 
         init();
     }
