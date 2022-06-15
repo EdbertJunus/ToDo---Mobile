ae0a9f47a90487acb5bb6c39a809a46f79323057
Kevin Tan <48116781+kevintannn@users.noreply.github.com>
2022-06-16 00:20:22 +0700

    conflict

--- app/src/main/java/com/example/todomobile/MainActivity.java
+++ app/src/main/java/com/example/todomobile/MainActivity.java
@@ -11,11 +11,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
-<<<<<<<<< Temporary merge branch 1
 import com.google.firebase.firestore.FirebaseFirestore;
 
-=========
->>>>>>>>> Temporary merge branch 2
 import java.util.HashMap;
 import java.util.Map;
 
@@ -28,37 +25,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         db = FirebaseFirestore.getInstance();
-<<<<<<<<< Temporary merge branch 1
         Map<String, Object> insertedData = new HashMap<>();
         insertedData.put("name", "Kevin");
-=========
 
         Map<String, Object> insertedData = new HashMap<>();
         insertedData.put("name", "WAKANDA");
->>>>>>>>> Temporary merge branch 2
         insertedData.put("age", 21);
         insertedData.put("gender", "Male");
 
         //Jika nama collectionPath asal, dia auto kebuat di db
         //Jika db tidak diterima, dia auto id
-<<<<<<<<< Temporary merge branch 1
         //        db.collection("students")
         //                .document("2201767623")
         //                .set(insertedData);
-=========
 //        db.collection("students")
 //                .document("2201767623")
 //                .set(insertedData);
->>>>>>>>> Temporary merge branch 2
         db.collection("students")
                 .document()
                 .set(insertedData);
 
-<<<<<<<<< Temporary merge branch 1
         //db.collection("students").add(insertedData);
 
-=========
         db.collection("students").add(insertedData);
->>>>>>>>> Temporary merge branch 2
     }
 }
